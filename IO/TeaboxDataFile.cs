61ec8e636db276ce636bcbb55ee0b6da9bc8671a
Henrik <Henrik@Matrix.local>
2017-02-18 12:59:44 +0100

    New feature for auto seting values on classes
    Added Experimental feature for making life easier (I hope)

--- IO/TeaboxDataFile.cs
+++ IO/TeaboxDataFile.cs
@@ -56,6 +56,11 @@ namespace TeaboxDataFormat.IO
             return new_file;
         }
 
+        /// <summary>
+        /// ToDo
+        /// By Reference
+        /// </summary>
+        /// <returns></returns>
         public TeaboxDataTable GetData()
         {
             var result = new TeaboxDataTable(_titles);
@@ -70,6 +75,56 @@ namespace TeaboxDataFormat.IO
                     result.Add(datarow);
                 }
                     
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// ToDo
+        /// </summary>
+        /// <typeparam name="output_type"></typeparam>
+        /// <returns></returns>
+        public IEnumerable<output_type> GetDataAs<output_type>() where output_type : TeaboxDataLine, new()
+        {
+            var result = new List<output_type>();
+
+            foreach (var line in _lines)
+            {
+                if (TeaboxDataLine.GetLineType(line) == TeaboxDataLineType.Data)
+                {
+                    var new_item = new output_type();
+                    TeaboxDataLine.SetData(new_item, TeaboxDataLine.GetData(line));
+                    TeaboxDataLine.SetTitles(new_item, TeaboxDataLine.GetTitles(line));
+
+                    foreach (var prop in new_item.GetType().GetProperties())
+                    {
+                        var atts = prop.GetCustomAttributes(typeof(TeaboxDataAttribute), false);
+
+                        if (atts.Length == 1 && atts[0].GetType() == typeof(TeaboxDataAttribute))
+                        {
+                            if (prop.PropertyType == typeof(string))
+                                prop.SetValue(new_item, TeaboxDataLine.GetData(line, prop.Name));
+                            else if (prop.PropertyType == typeof(int))
+                            {
+                                int v = 0;
+                                int.TryParse(TeaboxDataLine.GetData(line, prop.Name), out v);
+                                prop.SetValue(new_item, v);
+                            }
+                            else if (prop.PropertyType == typeof(DateTime))
+                            {
+                                DateTime v = DateTime.MinValue;
+                                DateTime.TryParse(TeaboxDataLine.GetData(line, prop.Name), out v);
+                                prop.SetValue(new_item, v);
+                            }
+                            else
+                                throw new Exception("Property type not supported.");
+                        }
+                    }
+
+                    result.Add(new_item);
+                }
+
             }
 
             return result;
--- Tests/Experiments.cs
+++ Tests/Experiments.cs
@@ -16,8 +16,99 @@ namespace Tests
     [TestFixture]
     public class Experiments
     {
+        [Test]
+        public void GetDataAsWorksWithStringProperties()
+        {
+            var file = new Mock<IFileContainer>();
+            file.Setup(x => x.ReadAllLines()).Returns(new List<string>()
+                {
+                    "!DateField1\tDateField2",
+                    "Hello\tWorld",
+                    "// A bit of a comment", 
+                    "Something\tElse",
+                });
 
+            var data_file = TeaboxDataFile.Open(file.Object);
+            var result = data_file.GetDataAs<TestItemForGetDataAsWorksWithStringProperties>();
+
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result.Count(x => x.DateField1 == "Hello" && x.DateField2 == "World"), Is.EqualTo(1));
+            Assert.That(result.Count(x => x.DateField1 == "Something" && x.DateField2 == "Else"), Is.EqualTo(1));
+        }
+
+        public class TestItemForGetDataAsWorksWithStringProperties : TeaboxDataLine
+        {
+            [TeaboxData]
+            public string DateField1 { get; set; }
+            [TeaboxData]
+            public string DateField2 { get; set; }
+        }
+
+        [Test]
+        public void GetDataAsWorksWithIntAndDateTimeProperties()
+        {
+            var file = new Mock<IFileContainer>();
+            file.Setup(x => x.ReadAllLines()).Returns(new List<string>()
+                {
+                    "!MyRowID\tMyRowData\tMyEditDate",
+                    "11\tHello\t2017-02-11",
+                    "// A bit of a comment",
+                    "22\tWorld\t2017-01-12",
+                });
+
+            var data_file = TeaboxDataFile.Open(file.Object);
+            var result = data_file.GetDataAs<TestItemForGetDataAsWorksWithIntAndDateTimeProperties>();
+
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result.Count(x => x.MyRowID == 11 && x.MyEditDate == new DateTime(2017, 2, 11)), Is.EqualTo(1));
+            Assert.That(result.Count(x => x.MyRowID == 22 && x.MyEditDate == new DateTime(2017, 1, 12)), Is.EqualTo(1));
+        }
+
+        public class TestItemForGetDataAsWorksWithIntAndDateTimeProperties : TeaboxDataLine
+        {
+            [TeaboxData]
+            public int MyRowID { get; set; }
+            [TeaboxData]
+            public DateTime MyEditDate { get; set; }
+        }
+
+        [Test, Explicit]
+        public void CodeForIdentifingCorrectlyMarkedProperties()
+        {
+            var foo = new FileRowItem { };
+            foreach (var prop in foo.GetType().GetProperties())
+            {
+                Console.WriteLine("{0}={1}", prop.Name, prop.GetValue(foo, null));
+
+                var atts = prop.GetCustomAttributes(typeof(TeaboxDataAttribute), false);
+
+                if (atts.Length == 1 && atts[0].GetType() == typeof(TeaboxDataAttribute))
+                    Console.WriteLine("TeaboxDataAttribute: Yes");
+                else
+                    Console.WriteLine("TeaboxDataAttribute: No");
+
+                if(prop.PropertyType == typeof(string))
+                    Console.WriteLine("Type: string");
+                if (prop.PropertyType == typeof(int))
+                    Console.WriteLine("Type: int");
+                if (prop.PropertyType == typeof(DateTime))
+                    Console.WriteLine("Type: DateTime");
+            }
+        }
     }
+
+    
+
+    public class FileRowItem : TeaboxDataLine
+    {
+        //[TeaboxData]
+        public int MyRowID { get; set; }
+        [TeaboxData]
+        public string MyRowData { get; set; }
+        //[TeaboxData]
+        public DateTime MyEditDate { get; set; }
+    }
+    
 }
 
 
